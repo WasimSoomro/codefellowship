3a6845a1c3487ffbbedb12dbf2e399bb4ed9e9e4
Wasim Soomro <103798599+WasimSoomro@users.noreply.github.com>
2023-08-01 19:03:07 -0400

    Initial Commit

---
+++ src/test/java/com/Lab16/Lab16/Lab16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lab16.Lab16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
